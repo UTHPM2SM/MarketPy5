917334c56d1f2ee2af6eb7ba5e4cd20d4a40aad8
JosueSauceda <106897370+JosueSauceda@users.noreply.github.com>
2023-04-11 22:08:09 -0600

    Corrección en Productos

--- MarketPy5/MarketPy5/Controlador/ProductosControlador.cs
+++ MarketPy5/MarketPy5/Controlador/ProductosControlador.cs
@@ -110,7 +110,7 @@ namespace MarketPy5.Controlador
         //==============================================================================
         public static async Task<bool> DeleteProduct(string id)
         {
-            await firebaseClient.Child(nameof(Productos) + "/" + id).DeleteAsync();
+            await firebaseClient.Child("Products" + id).DeleteAsync();
             return true;
         }
 
@@ -119,7 +119,7 @@ namespace MarketPy5.Controlador
         //==============================================================================
         public static async Task<bool> UpdateProduct(Productos product)
         {
-            await firebaseClient.Child(nameof(Productos) + "/" + product.Id).PutAsync(JsonConvert.SerializeObject(product));
+            await firebaseClient.Child("Products" + "/" + product.Id).PutAsync(JsonConvert.SerializeObject(product));
             return true;
         }
 
